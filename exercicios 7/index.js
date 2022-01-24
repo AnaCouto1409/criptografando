72ae376a5a86e22b5d59102dd6f461d5e6c544d5
AnaCouto1409 <35375044+AnaCouto1409@users.noreply.github.com>
2022-01-24 19:23:18 -0300

    Update index.js

--- exercicios 7/index.js
+++ exercicios 7/index.js
@@ -22,11 +22,35 @@ var chave = document.querySelector("#chave");
  select.addEventListener('change',  function(){
      if(select.value ==="cifra"){
          ocultar.style.display= "block";
+       document.querySelector("body").style.backgroundColor = "#e5fafa";
      }else{
          ocultar.style.display = "none";
+      document.querySelector("body").style.backgroundColor = "#F0FFF0";
      }
  });
 
+
+//pegando o q o usuario digitou e atribuindo nas funções base64.js e cifraCesar.js, e retornando o resultado
+enviar.addEventListener('click', function () {
+    var texto = digite.value;
+
+    if (criptografar.checked) { //se o radio buttom criptografar estiver selecionado ele faz >>
+        if (selecionado.value == 'cifra') {
+            resultado.value = cifraCesar(texto); //pega valor digitado e coloca no resultado
+        }
+        else {
+            resultado.value = base64(texto);//pega valor digitado e coloca no resultado
+        }
+    }
+    else {
+        if (selecionado.value == 'cifra') {
+            resultado.value = decodifCesar(texto); //mesma coisa q acima, só q com a função de descodificar
+        }
+        else {
+            resultado.value = decodif64(texto);
+        }
+    }
+});
 
 
 
